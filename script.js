daeefd30b3567054b091b2cb4d926d33897c1bf6
Andrea <134276451+andreacasonato@users.noreply.github.com>
2025-07-09 17:57:10 +0200

    Add premium functionality, reset

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+const counterEl = document.querySelector('.counter');
 const increaseButtonEl = document.querySelector('.counter__button--increase');
 const decreaseButtonEl = document.querySelector('.counter__button--decrease');
 const resetButtonEl = document.querySelector('.counter__reset-button');
 const counterValueEl = document.querySelector('.counter__value');
+const counterTitleEl = document.querySelector('.counter__title')
+
 
 resetButtonEl.addEventListener('click', function() {
     // set counter value to 0
     counterValueEl.textContent = 0;
+
+    // reset background color
+    counterEl.classList.remove('counter--limit');
+
+    // reset counter title
+    counterTitleEl.textContent = 'Fancy Counter';
+
+    // enable increase and decrease buttons
+    increaseButtonEl.disabled = false;
+    decreaseButtonEl.disabled = false;
+
+    // unfocus (blur) reset button
+    resetButtonEl.blur();
 });
 
 decreaseButtonEl.addEventListener('click', function() {
@@ -26,6 +42,9 @@ decreaseButtonEl.addEventListener('click', function() {
 
     // update counter value with new value
     counterValueEl.textContent = newValue;
+
+    // unfocus (blur) reset button
+    resetButtonEl.blur();
 });
 
 function incrementCounter () {
@@ -37,10 +56,29 @@ function incrementCounter () {
     const currentValueAsNumber = +currentValue;
 
     // increment by 1
-    const newValue = currentValueAsNumber + 1;
+    let newValue = currentValueAsNumber + 1;
+
+    // check if new value is greater than 5
+    if (newValue > 5) {
+        // if it is, keep it 5
+        newValue = 5;
+
+        // give visual indicator that limit has been reached
+        counterEl.classList.add('counter--limit');
+
+        //update counter title to say limit has been reached
+        counterTitleEl.innerHTML = 'Limit! Buy <b>Pro</b> for >5';
+
+        // disable both increase and decrease when limit has been reached
+        increaseButtonEl.disabled = true;
+        decreaseButtonEl.disabled = true;
+    }
 
     // set counter element with new value
     counterValueEl.textContent = newValue;
+
+    // unfocus (blur) reset button
+    resetButtonEl.blur();
 }
 
 increaseButtonEl.addEventListener('click', incrementCounter);
